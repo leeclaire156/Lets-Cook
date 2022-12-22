8f9d672f11348ca1cb0f157ff85c7a8b04f604d3
VenBak <andre.brahin@gmail.com>
2022-12-22 02:58:02 +0100

    renders card for api search results and opens modal when user click

--- Develop/Assets/displayed-results.js
+++ Develop/Assets/displayed-results.js
@@ -60,14 +60,68 @@ function resultsLogged() {
     var params = new URLSearchParams(userSearch);
     var food = params.get("q");
     console.log(food);
-    var resultsUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=d1ae693fc9a54390aeaf4500f67b3932&ingredients=" + food;
+    var resultsUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=c1f7340594f64ceb9978a2493dfcf2cb&ingredients=" + food;
     fetch(resultsUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-        })
-}
+            for (let index = 0; index < 10; index++) {
+    // Renders Card when function is called in an area called content
+    var column = document.createElement("div");
+    var cardpage = document.querySelector(".columns");
+    cardpage.append(column);
+    column.setAttribute("class", "column is-one-quarter");
+    var card = document.createElement("div");
+    column.append(card);
+    card.setAttribute("class", "card")
+    var cardcontent = document.createElement("div");
+    card.append(cardcontent);
+    cardcontent.setAttribute("class", "card-content");
+    var content = document.createElement("div");
+    cardcontent.append(content);
+    content.setAttribute("class", "content");
+    var title = document.createElement("p");
+    content.append(title);
+    title.setAttribute("class", "title is-4");
+    // var duration = document.createElement("p");
+    // content.append(duration);
+    // duration.setAttribute("id", "duration")
+    var ingredients = document.createElement("p");
+    content.append(ingredients);
+    ingredients.setAttribute("id", "ingredients");
+    var cardimage = document.createElement("div");
+    card.append(cardimage);
+    cardimage.setAttribute("class", "card-image");
+    var figure = document.createElement("figure");
+    cardimage.append(figure);
+    figure.setAttribute("class", "image is-4by3")
+    var img = document.createElement("img");
+    figure.append(img)
+    var footer = document.createElement("footer");
+    card.append(footer);
+    footer.setAttribute("class", "card-footer");
+    var anchor = document.createElement("a");
+    footer.append(anchor);
+    anchor.setAttribute("class", "js-modal-trigger card-footer-item");
+    anchor.setAttribute("data-target", "modalinfo");
+    anchor.setAttribute("id", "moreinfo")
+    
+    title.innerHTML = data[index].title
+    // duration.innerHTML = "45 minutes"
+    ingredients.innerHTML = "Missing " + data[index].missedIngredients.length + " Ingredients" 
+    img.setAttribute("src", data[index].image)
+    anchor.innerHTML = "Open for more"
+
+    // Opens modal when somebody presses open for more
+    anchor.addEventListener("click", function () {  
+        var modal = document.querySelector("#modalinfo")
+        modal.removeAttribute("class");
+        modal.setAttribute("class", "modal is-active")
+    })
+    }
+})}
+
 
 resultsLogged();
